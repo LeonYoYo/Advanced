d78211fb774753c31e3c7ecdaf6fd9d81eb221a3
陈陈 <7900868+chenscup@user.noreply.gitee.com>
2020-09-14 16:53:17 +0800

    add: 01-02

---
+++ fed-e-task-01-02/code/app.js
@@ -0,0 +1,32 @@
+// 1
+// var a = [];
+// for (var i = 0; i < 10; i++) {
+//   a[i] = function () {
+//     console.log(i)
+//   }
+// }
+// a[6]()
+
+
+//2
+// var tmp = 123;
+// if (true) {
+//   console.log(tmp)
+//   let tmp
+// }
+
+//3
+// var arr = [12, 34, 32, 89, 4];
+
+
+// 5
+// var a = 10;
+// var obj = {
+//   a: 20,
+//   fn() {
+//     setTimeout(() => {
+//       console.log(this.a)
+//     })
+//   }
+// }
+// obj.fn()
