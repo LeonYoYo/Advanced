70d97d07ca09042dce1b86660738ef8510336ccf
陈陈 <7900868+chenscup@user.noreply.gitee.com>
2020-09-27 17:56:23 +0800

    feat: Generator-自定义nodejs脚手架

---
+++ fed-e-task-02-01/code/generator-practice-proj/generators/app/index.js
@@ -0,0 +1,33 @@
+const Generator = require('yeoman-generator')
+
+module.exports = class extends Generator {
+  prompting () {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.appname
+      }
+    ])
+    .then(answers => {
+      this.answers = answers
+    })
+  }
+
+  writing () {
+
+    const templates = [
+      'index.html',
+      'style.css',
+    ]
+
+    templates.forEach(item => {
+      this.fs.copyTpl(
+        this.templatePath(item),
+        this.destinationPath(item),
+        this.answers
+      )
+    })
+  }
+}
